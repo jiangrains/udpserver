c7d2abee274d572422ed47551939832f2f80a35c
jiangrains <jiangrains@126.com>
2016-06-15 17:40:29 +0800

    Update udpclient.py

--- udpclient.py
+++ udpclient.py
@@ -1,14 +1,16 @@
 # --*-- coding:utf-8 -*-
 import socket
+import struct
 
-address = ('127.0.0.1', 10000)
+address = ('192.168.1.12', 10000)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while True:
-	msg = raw_input()
-	print msg
-	if not msg:
-		break
-	s.sendto(msg, address)
+length = 24
+INSOLE_VERSION = 0x1
+PACKET_TYPE_REGISTER_RESPONSE = 0x1
+sn = "#000000000000001"
+PACKET_MAGICNUM = 0xffffffff
+register_packet = struct.pack("!H2b16sI", length, INSOLE_VERSION, PACKET_TYPE_REGISTER_RESPONSE, sn, PACKET_MAGICNUM)
+s.sendto(register_packet, address)
 
 s.close()  
