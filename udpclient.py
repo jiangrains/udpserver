e1677ded4d74d6235c2a6d9867a4748c30c4957b
jiangrains <jiangrains@126.com>
2016-06-16 14:40:28 +0800

    Update udpclient.py

--- udpclient.py
+++ udpclient.py
@@ -8,9 +8,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 length = 24
 INSOLE_VERSION = 0x1
 PACKET_TYPE_REGISTER_RESPONSE = 0x1
-sn = "#000000000000001"
+sn = "#000000000000002"
 PACKET_MAGICNUM = 0xffffffff
 register_packet = struct.pack("!H2b16sI", length, INSOLE_VERSION, PACKET_TYPE_REGISTER_RESPONSE, sn, PACKET_MAGICNUM)
 s.sendto(register_packet, address)
 
 s.close()  
+
